5dfce3e4e12af09625ef9053fa53f2bf81979e9f
mo.elsayed213 <121774151+mohamed21320@users.noreply.github.com>
2024-02-20 15:31:39 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,335 @@
+/* var input=5;
+console.log(input); */
+// -------------------------------------------------
+
+/* var input= +window.prompt("enter number please");
+if (input%3==0 && input%4==0) {
+    console.log("Yes");
+} else {
+    console.log("No"); 
+} */
+// ---------------------------------------------------
+
+/* var input= +window.prompt("enter number please");
+var input1= +window.prompt("enter number 2 please");
+if (input > input1 ) {
+    console.log(input);
+} else {
+    console.log(input1); 
+} */
+// ---------------------------------------------------
+
+/* var input= +window.prompt("enter number please");
+if (input>0) {
+    console.log("Possitive");
+}
+else{
+    console.log("Negative");
+} */
+// ---------------------------------------------------
+
+/* var input = +window.prompt("enter number please");
+var input1= +window.prompt("enter number please");
+var input2= +window.prompt("enter number please");
+
+if (input > input1 && input > input2) {
+    console.log( "max Elememt=" , input);
+} else if (input1 > input2 && input1 > input ) {
+    console.log("max Elememt=" ,  input1);
+}
+else if(input2 > input && input2 > input1){
+    console.log( "max Elememt=" , input2);
+}
+else{
+    console.log("noooooooo"); 
+}
+if (input < input1 && input < input2) {
+    console.log( "min Elememt=" , input);
+}
+else if (input1 < input2 && input1 < input ) {
+    console.log("min Elememt=" ,  input1);
+}
+else if(input2 < input && input2 < input1){
+    console.log( "min Elememt=" , input2);
+}else{
+    console.log("noooooooo"); 
+} */
+// ---------------------------------------------------
+
+/* var input= +window.prompt("Enter Number Please");
+if (input%2==0) {
+    console.log("Even");
+}else{
+    console.log("Odd");
+} */
+// ---------------------------------------------------
+
+/* var char =window.prompt("Enter character Please");
+if (char=='a'||char=='e'||char=='i'||char=='o'||char=='u'||char=='A'||char=='E'|| char=='I'||char=='O'||char=='U') {
+    console.log("Vowel");
+}else{
+    console.log("consonant");
+} */ 
+// ---------------------------------------------------
+ 
+/* var input= +window.prompt("Enter Number Please");
+for( var i=1   ; i<=input  ;  i++  ){
+console.log(i);
+} */
+// ---------------------------------------------------
+
+/* var input= +window.prompt("Enter Number Please");
+for( var i=1   ; i<=12  ;  i++  ){
+console.log(i*input);
+} */
+// ---------------------------------------------------
+
+/* var input= +window.prompt("Enter Number Please");
+for( var i=1  ; i<input  ; i++ ){
+if (i%2==0) {
+    console.log(i);
+}
+} */
+// ---------------------------------------------------
+
+/* var input= +window.prompt("Enter Number Please");
+var input1= +window.prompt("Enter Number Please");
+
+for(var i=0  ; i<input1 ; i++ ){
+    console.log(input**input1);
+}
+ */
+// ---------------------------------------------------
+
+/* for( var i=0 ;i<5  ;i++  ){
+    var input= +window.prompt("Enter Number Please");
+    var sum=  input+input;
+    var ave= sum / 5;
+}
+console.log(sum);
+console.log(ave); */
+// ---------------------------------------------------
+
+/* var input= +window.prompt("Enter Number Please");
+switch (input) {
+    case 1:
+        console.log("Days in month:31");
+        break;
+        case 1:
+            console.log("Days in month:31");
+            break;
+        case 2:
+        console.log("Days in month:28");
+             break;
+        case 3:
+        console.log("Days in month:31");
+            break;
+        case 4:
+        console.log("Days in month:30");
+            break;
+        case 5:
+        console.log("Days in month:31");
+            break;
+        case 6:
+        console.log("Days in month:30");
+            break;
+        case 7:
+        console.log("Days in month:31");
+            break;
+        case 8:
+        console.log("Days in month:31");
+            break;
+        case 9:
+        console.log("Days in month:30");
+            break;
+        case 10:
+        console.log("Days in month:31");
+            break;
+        case 11:
+        console.log("Days in month:30");
+            break;
+        case 12:
+        console.log("Days in month:31");
+            break;
+    default:
+        console.log("IT's not month");
+        break;
+} */
+// ---------------------------------------------------
+
+/* var precentage=+window.prompt('Enter precent Please')
+if(precentage>=90){
+console.log("Grad A");
+}
+else if(precentage>=80){
+    console.log("Grad B");
+}
+else if(precentage>=70){
+    console.log("Grad C");
+}
+else if(precentage>=60){
+    console.log("Grad D");
+}
+else if(precentage>=40){
+    console.log("Grad E");
+}
+else {
+    console.log("Grad F");
+} */
+// ---------------------------------------------------
+   
+/////////////////////////////////////////////////////// switch///////////////////////////////////////////
+
+
+/* var input= +window.prompt("Enter Number Please");
+switch (input) {
+    case 1:
+        console.log("Days in month:31");
+        break;
+        case 1:
+            console.log("Days in month:31");
+            break;
+        case 2:
+        console.log("Days in month:28");
+             break;
+        case 3:
+        console.log("Days in month:31");
+            break;
+        case 4:
+        console.log("Days in month:30");
+            break;
+        case 5:
+        console.log("Days in month:31");
+            break;
+        case 6:
+        console.log("Days in month:30");
+            break;
+        case 7:
+        console.log("Days in month:31");
+            break;
+        case 8:
+        console.log("Days in month:31");
+            break;
+        case 9:
+        console.log("Days in month:30");
+            break;
+        case 10:
+        console.log("Days in month:31");
+            break;
+        case 11:
+        console.log("Days in month:30");
+            break;
+        case 12:
+        console.log("Days in month:31");
+            break;
+    default:
+        console.log("IT's not month");
+        break;
+} */
+
+// ---------------------------------------------------
+ 
+/* var character=+window.prompt('Enter char Please')
+switch (character) {
+    case 'a':
+        console.log('vowel');
+        break;
+        case 'a':
+            console.log('vowel');
+            break;
+        case 'e':
+        console.log('vowel');
+        break;
+        case 'i':
+        console.log('vowel');
+        break;
+        case 'o':
+        console.log('vowel');
+        break;
+        case 'u':
+        console.log('vowel');
+        break;
+        case 'A':
+        console.log('vowel');
+        break;
+        case 'E':
+        console.log('vowel');
+        break;
+        case 'I':
+        console.log('vowel');
+        break;
+        case 'O':
+        console.log('vowel');
+        break;
+        case 'U':
+        console.log('vowel');
+        break;
+
+    default:
+        console.log('consonant');
+        break;
+} */
+// ---------------------------------------------------
+ 
+/* var input=+window.prompt('Enter number Please')
+var input1=+window.prompt('Enter number Please')
+switch (true) {
+    case (input>input1) :
+        console.log("max=" , input);
+        break;
+
+    default:
+        console.log("max=" , input1);
+        break;
+} */
+// ---------------------------------------------------
+ 
+/* var input=+window.prompt('Enter number Please')
+switch (true) {
+    case (input%2==0) :
+        console.log("even");
+        break;
+
+    default:
+        console.log("odd");
+        break;
+} */
+// ---------------------------------------------------
+ 
+/* var input=+window.prompt('Enter number Please')
+switch (true) {
+    case (input==0) :
+        console.log("zeroo");
+        break;
+        case (input>0) :
+            console.log("positive");
+            break;
+        ;
+    default:
+        console.log("negative");
+        
+} */
+// ---------------------------------------------------
+ 
+/* var input=+window.prompt('Enter number Please')
+var char=window.prompt('Enter char Please')
+var input1=+window.prompt('Enter number Please')
+switch (char) {
+    case "+" :
+        console.log(input+ input1);
+        break;
+        case "-" :
+            console.log(input- input1);
+            break;
+            case "*" :
+        console.log(input* input1);
+        break;
+        case "/" :
+        console.log(input/ input1);
+        break;
+        
+    default:
+        console.log("no result");
+        break;
+} */
+
